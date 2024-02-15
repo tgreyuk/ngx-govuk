bb0c9fd62727e9e1d3085ecc6ddd60a2dc83c602
tgreyuk <tgreyuk@gmail.com>
2024-02-15 22:09:46 +0000

    fix: Updated character count to use input signals

--- libs/ngx-govuk/forms/src/character-count/character-count.component.stories.ts
+++ libs/ngx-govuk/forms/src/character-count/character-count.component.stories.ts
@@ -1,4 +1,4 @@
-import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta } from '@storybook/angular';
 
 import { NgxGovukFormGroupComponent } from '../form-group/form-group.component';
 import { NgxGovukTextareaDirective } from '../textarea/textarea.directive';
@@ -14,14 +14,16 @@ const meta: Meta<NgxGovUkCharacterCountComponent> = {
   ],
 };
 export default meta;
-type Story = StoryObj<NgxGovUkCharacterCountComponent>;
-
-export const Preview: Story = {
+//type Story = StoryObj<NgxGovUkCharacterCountComponent>
+// Currently unable to type Preview: Story - See https://github.com/storybookjs/storybook/issues/25784
+export const Preview = {
   args: {
     limit: 10,
   },
-  render: (args) => ({
-    props: args,
+  render: (args: NgxGovUkCharacterCountComponent) => ({
+    props: {
+      limit: args.limit,
+    },
     template: `
 <ngx-govuk-character-count [limit]="limit">
   <ngx-govuk-form-group label="Can you provide more detail?" hint="Do not include personal or financial information like your National Insurance number or credit card details." >
