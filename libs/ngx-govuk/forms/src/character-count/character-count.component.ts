b13018e398e031e77ca6cd54d2f284a61cc6b299
tgreyuk <tgreyuk@gmail.com>
2024-02-15 14:54:53 +0000

    fix: updated character count component

--- libs/ngx-govuk/forms/src/character-count/character-count.component.ts
+++ libs/ngx-govuk/forms/src/character-count/character-count.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import {
   AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   ContentChild,
   Input,
   Renderer2,
+  computed,
+  effect,
   signal,
 } from '@angular/core';
 import { NgxGovukTextareaDirective } from '../textarea/textarea.directive';
@@ -15,28 +18,45 @@ import { NgxGovukTextareaDirective } from '../textarea/textarea.directive';
   imports: [CommonModule],
   templateUrl: './character-count.component.html',
   styleUrl: './character-count.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NgxGovUkCharacterCountComponent implements AfterViewInit {
-  @Input() limit = 200;
+  @Input() set limit(limit: number) {
+    this.maxLength.set(limit);
+  }
 
   @ContentChild(NgxGovukTextareaDirective)
   textarea!: NgxGovukTextareaDirective;
 
   count = signal(0);
+  maxLength = signal(0);
+  remainingCount = computed(() => this.maxLength() - this.count());
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) {
+    effect(() => {
+      this.setMaxLength();
+    });
+  }
 
   ngAfterViewInit(): void {
-    this.count.set(this.limit);
-    this.renderer.setAttribute(
-      this.textarea.field,
-      'maxLength',
-      this.limit.toString()
-    );
     this.renderer.listen(this.textarea.field, 'input', () => {
-      const newCount = this.limit - this.textarea.field.value.length;
-
-      this.count.set(newCount);
+      this.count.set(this.textarea.field.value.length);
     });
+  }
+
+  setMaxLength(): void {
+    if (this.textarea.field.maxLength != this.maxLength()) {
+      if (this.textarea.field.value) {
+        this.textarea.field.value = this.textarea.field.value.substring(
+          0,
+          this.maxLength()
+        );
+      }
+      this.renderer.setAttribute(
+        this.textarea.field,
+        'maxLength',
+        this.maxLength().toString()
+      );
+    }
   }
 }
