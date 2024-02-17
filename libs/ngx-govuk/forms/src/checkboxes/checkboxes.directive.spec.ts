17957810eec1831a4766ed4c6b3ac9c08d65ef45
tgreyuk <tgreyuk@gmail.com>
2024-02-17 19:59:52 +0000

    feat: Added checkbox component

---
+++ libs/ngx-govuk/forms/src/checkboxes/checkboxes.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CheckboxesDirective } from './checkboxes.directive';
+
+describe('CheckboxesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CheckboxesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
