9ef796ffddb09adb7a9f1bac75e941689bf8598f
tgreyuk <tgreyuk@gmail.com>
2024-02-22 10:03:48 +0000

    chore-releaseit

--- libs/ngx-govuk/.storybook/preview.ts
+++ libs/ngx-govuk/.storybook/preview.ts
@@ -1,7 +1,4 @@
-import { setCompodocJson } from '@storybook/addon-docs/angular';
 import { Preview } from '@storybook/angular';
-import docJson from './documentation.json';
-setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
