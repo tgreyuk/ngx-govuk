e97e91ad012d44578914d799f0fdd947a7e4e349
tgreyuk <tgreyuk@gmail.com>
2024-02-28 14:34:16 +0000

    chore: Updated packages

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ngx-govuk
 
-![npm](https://img.shields.io/npm/v/ngx-govuk?&logo=npm)
+![npm](https://img.shields.io/npm/v/ngx-govuk?&logo=npm) [![Build Status](https://github.com/tgreyuk/ngx-govuk/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/tgreyuk/ngx-govuk/actions/workflows/ci.yml)
 
 [GOV.UK Design System](https://design-system.service.gov.uk) components for Angular projects.
 
--- libs/ngx-govuk/button/button.directive.ts
+++ libs/ngx-govuk/button/button.directive.ts
@@ -44,7 +44,6 @@ export class NgxGovukButton {
     private renderer: Renderer2,
   ) {
     effect(() => {
-      console.log(this.buttonType());
       if (this.buttonType() === 'start') {
         const svgContainer = this.renderer.createElement('div');
         svgContainer.innerHTML = `<svg class="govuk-button__start-icon" xmlns="http://www.w3.org/2000/svg" width="17.5" height="19" viewBox="0 0 33 40" aria-hidden="true" focusable="false">
